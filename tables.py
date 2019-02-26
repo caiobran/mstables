b5fe700f32f7f0f7075cd1690bb5084cf3384de9
caiobran <caiobran88@gmail.com>
2019-02-25 20:09:45 -0600

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	deleted:    tables.py
    
     Untracked files:
    	.gitignore

--- tables.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class tables():
-
-    def __init__(self, json_file):
-        with open(json_file) as file:
-            self.js = json.load(file)
-        self.names = list(self.js.keys())
